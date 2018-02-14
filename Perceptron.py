e6cd069642a7e65b3b868a45723ecaf98ac1e340
Dan Whettam <33871216+DWhettam@users.noreply.github.com>
2018-02-14 16:05:06 +0000

    Read in data

--- Perceptron.py
+++ Perceptron.py
@@ -1,39 +1,27 @@
-import numpy as np
-import csv
+
+import pandas as pd
 import random
 import math
 
+
 def sigmoid(x):
-  return 1 / (1 + math.exp(-x))
+    return 1 / (1 + math.exp(-x))
+
 
 def load_data(filename):
+        df = pd.read_csv(filename)
+        labels = df.iloc[:, -1]
+        features = df.iloc[:, :-1]
+        print("data:", df)
+        print("features", features)
 
-        label = []
-        data = []
+        return features, labels
 
-        data = np.genfromtxt(filename, delimeter = ',', dtype = None)
-        
-        for row in csv_file:                        
-                        value = row[:-1]
-                        # Adds out the class value
-                        data.append(value)
 
-                        # Removes the label and saves the data
-                        del row[:-1]
-                        label.append(row) 
-                    
-                        
-        return data, label
-        
 x_test, y_test = load_data('TestData.csv')
-x_train, y_train = load_data('TrainingData.csv')
-
-n = len(x_test[0])
 learning_rate = 0.02
 weights = []
 
-for i in range(n):
-        weights.append(round(random.uniform(-0.5,0.5),2))
 
 y_predict = []
 
