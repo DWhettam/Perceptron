08553a91c122c236fec4847f74105184f1930872
Dan Whettam <33871216+DWhettam@users.noreply.github.com>
2018-02-15 10:15:40 +0000

    Completed pecerptron logic

--- Perceptron.py
+++ Perceptron.py
@@ -1,28 +1,33 @@
-
+import numpy as np
 import pandas as pd
-import random
-import math
 
 
 def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
 def load_data(filename):
         df = pd.read_csv(filename)
         labels = df.iloc[:, -1]
         features = df.iloc[:, :-1]
-        print("data:", df)
-        print("features", features)
-
         return features, labels
 
 
 x_test, y_test = load_data('TestData.csv')
-learning_rate = 0.02
-weights = []
 
+learning_rate = 0.000002
+n = x_test.shape[1]
+weights = np.random.uniform(-0.5, 0.5, n)
+error = []
 
-y_predict = []
+while True:
+    previous_error = error
+    prediction = sigmoid(np.dot(x_test, weights))
+    error = y_test - prediction
+    delta = (np.dot(x_test.T, error) * learning_rate)
+    weights += delta
+    if np.array_equal(previous_error, error):
+        break
 
-print(y_test)
+print(weights)
+
