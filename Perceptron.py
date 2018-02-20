99cfda0d4afd29578c49568381db1a5a972797d0
Dan Whettam <dwhettam@gmail.com>
2018-02-20 16:53:39 +0000

    Updated to use correct data

--- Perceptron.py
+++ Perceptron.py
@@ -10,24 +10,23 @@ def load_data(filename):
         df = pd.read_csv(filename)
         labels = df.iloc[:, -1]
         features = df.iloc[:, :-1]
-        return features, labels
+        return labels, labels
 
 
-x_test, y_test = load_data('TestData.csv')
+x_test, y_test = load_data('Initial_data.csv')
 
 learning_rate = 0.000002
-n = x_test.shape[1]
+n = x_test.shape[0]
 weights = np.random.uniform(-0.5, 0.5, n)
 error = []
+prediction = []
 
-while True:
+for i in range(10):
     previous_error = error
     prediction = sigmoid(np.dot(x_test, weights))
+    print(prediction)
     error = y_test - prediction
     delta = (np.dot(x_test.T, error) * learning_rate)
     weights += delta
-    if np.array_equal(previous_error, error):
-        break
 
 print("predictions: ", prediction)
-
