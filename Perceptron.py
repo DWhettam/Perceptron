5aaba53be3fefc9540dfe68e145188f8e9356152
Dan Whettam <dwhettam@gmail.com>
2018-03-02 12:17:40 +0000

    Added heaviside step-function

--- Perceptron.py
+++ Perceptron.py
@@ -5,7 +5,6 @@ from sklearn.model_selection import train_test_split
 
 def load_data(filename, num_columns):
         df = (pd.read_csv(filename)).iloc[:, -1]
-        #split data into train and test sets
         x_train, x_test = train_test_split(df, test_size = 0.3, random_state = None, shuffle = False, stratify = None)
         train_matrix = np.zeros((x_train.shape[0], num_columns), dtype = float)
         test_matrix = np.zeros((x_test.shape[0], num_columns), dtype = float)
@@ -15,6 +14,7 @@ def load_data(filename, num_columns):
             train_matrix[num_columns-(i+1):, i] = x_train[:-(num_columns-(i+1)) or None]
             test_matrix[num_columns-(i+1):, i] = x_test[:-(num_columns-(i+1)) or None]
             data_matrix[num_columns-(i+1):, i] = df[:-(num_columns-(i+1)) or None]
+        #Adding bias column
         train_matrix = np.c_[np.ones(train_matrix.shape[0]), train_matrix]
         test_matrix = np.c_[np.ones(test_matrix.shape[0]), test_matrix]
         data_matrix = np.c_[np.ones(data_matrix.shape[0]), data_matrix]
@@ -23,20 +23,12 @@ def load_data(filename, num_columns):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-# def step_function(x):
-#     threshold = 0.2
-#     true_false_values = np.all(np.less(np.subtract(x, threshold), 0))
-#     if np.etrue_false_values == True:
-#         return 1
-#     else:
-#         return 0
-
-
-
+def step_function(x):
+    threshold = 0.2
+    return np.heaviside(np.subtract(x, threshold), 0.5)
 
 x_train, x_test, x_complete = load_data('data.csv', 3)
 learning_rate = 0.5
-#initialise weights with random gaussian distribution
 weights = np.random.normal(0, 0.5, x_complete.shape[1])
 epochs = 100
 train_mse = []
@@ -74,13 +66,13 @@ plt1.set_xlabel("Iterations")
 plt1.set_ylabel("Robot Position")
 plt1.legend([predicted, actual], ['Predicted', 'Actual'])
 
-plt3 = plt.subplot2grid((2,3), (1,0), colspan=3)
-train, = plt3.plot(train_mse, label = 'Train')
-test, = plt3.plot(test_mse, label = 'Test')
-plt3.set_title("MSE Over Time")
-plt3.set_xlabel("Epochs")
-plt3.set_ylabel("Mean Squared Error")
-plt3.legend([test, train], ['Test', 'Train'])
+plt2 = plt.subplot2grid((2,3), (1,0), colspan=3)
+train, = plt2.plot(train_mse, label = 'Train')
+test, = plt2.plot(test_mse, label = 'Test')
+plt2.set_title("MSE Over Time")
+plt2.set_xlabel("Epochs")
+plt2.set_ylabel("Mean Squared Error")
+plt2.legend([test, train], ['Test', 'Train'])
 
 plt.tight_layout()
 plt.show()
